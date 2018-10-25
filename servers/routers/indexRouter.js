1265fce6ae506641f0d99f69104091686fe26b52
AndreiX7 <andrei.albiller@gmail.com>
2018-10-25 11:45:36 +0800

    Bug fixes

--- servers/routers/indexRouter.js
+++ servers/routers/indexRouter.js
@@ -4,14 +4,13 @@
 const express = require('express');
 const router = express.Router(); //eslint-disable-line
 
-<<<<<<< HEAD
 router.get('/', function getIndexPage(req, res) {
   let viewModel = req.viewModel;
   res.render('index.pug', viewModel);
-=======
+});
+
 router.get('/', (req, res) => {
   res.send('Online Shop');
->>>>>>> 5e49fab8147d3f6a57e114ecb42edd272d889d49
 });
 
 router.post('/', (req, res) => {
