192db6da9bec1f244881fb10d9acf9dcfdfb7c77
phengzkie <alfie.osayan@gmail.com>
2018-10-25 16:24:37 +0800

    update and delete router and vue

--- public/products-vue.js
+++ public/products-vue.js
@@ -8,6 +8,16 @@
       quantity: null,
       products: []
     },
+    created: function() {
+      var self = this;
+      axios.get('http://localhost:5000/api/products')
+        .then(res => {
+          self.products = res.data;
+        })
+        .catch(err => {
+          self.products = [];
+        });
+    },
     methods: {
       addProduct: function() {
         var self = this;
@@ -24,6 +34,37 @@
           })
           .catch(err => {
             console.log(err);
+          });
+      },
+      updateProduct: function(product) {
+        var self = this;
+        axios.put('/products' + product.id)
+          .then(res => {
+            for(var i = 0; i < self.products.length; i++) {
+              if(Number(self.products[i].id) === Number(product.id)) {
+                self.products = res.data;
+                break;
+              }
+            }
+          })
+          .catch(err => {
+            console.log(err);
+          });
+      },
+      deleteProduct: function(product) {
+        var self = this;
+        axios.delete('/products' + product.id)
+          .then(res => {
+            var index = -1;
+            for(var i = 0; i < self.product.length; ++i) {
+              if(Number(self.product[i].id) === Number(product.id)) {
+                index = i;
+                break;
+              }
+            }
+            self.product.splice(index, 1);
+          })
+          .catch(err => {
           });
       }
     }
